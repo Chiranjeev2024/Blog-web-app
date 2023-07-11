4948f1afe41711fe02d252e7b81ac83c378faad7
ssuyash3000 <ssuyash2001@gmail.com>
2023-07-11 14:17:58 +0530

    Implemented Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
 function Navbar() {
-  return <div className="Navbar">Navbar</div>;
+  return (
+    <div className="Navbar">
+      <ul id="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/createPost">Create Post</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
