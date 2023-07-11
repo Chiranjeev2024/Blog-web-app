8cd403e7c61c917b6430e2393231b91af47c34d4
ssuyash3000 <ssuyash2001@gmail.com>
2023-07-11 14:02:38 +0530

    Setting up routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,17 @@
+import "../Styles/App.css";
+import { Routes as Switch, Route } from "react-router-dom";
+import { Navbar, Home, PostDetail, CreatePost } from "./";
 function App() {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/postId" Component={PostDetail} />
+        <Route exact path="/createPost" Component={CreatePost} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
